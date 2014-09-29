137b9de4ccd5be35ce571c2812f9b288c2175a99
Ludovic Charleux <ludovic.charleux@univ-savoie.fr>
2014-09-29 15:54:26 +0200

    Updated models.py

--- models.py
+++ models.py
@@ -1,8 +1,22 @@
 # A repository for compartmentalized models compatible simulation models.
 
 
-def RingCompression(inner_radius, outer_radius, Nr, Nt):
+class RingCompression(object):
   """
-  Blablabli blablabla
+ Ring compression test.
   """
-  
+  def __init__(self, inner_radius, outer_radius, Nr, Nt):
+    """
+    :param inner_radius: inner radius of the ring
+    :type inner_radius: float
+    :param outer_radius: outer radius of the ring
+    :type outer_radius: float
+    :param Nr: Number of elements in the radial direction
+    :type Nr: int
+    :param Nt: Number of elements in the orthoradial direction
+    :type Nt: int
+    """
+    self.inner_radius = inner_radius
+    self.outer_radius = outer_radius
+    self.Nr = Nr
+    self.Nt = Nt
