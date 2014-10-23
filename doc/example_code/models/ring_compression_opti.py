a63c1d0ec8511f9fd32fa15ec02a186e0e2ed74e
lbizet <laurent.bizet@univ-savoie.fr>
2014-10-23 08:50:34 +0200

    Dimensionnal modifications

--- doc/example_code/models/ring_compression_opti.py
+++ doc/example_code/models/ring_compression_opti.py
@@ -14,12 +14,13 @@ node = platform.node()
 #FIXED PAREMETERS
 settings = {}
 settings['file_name'] = 'test_exp.txt'
-settings['inner_radius'], settings['outer_radius'] = 95/2, 100/2
+settings['inner_radius'], settings['outer_radius'] = 95./2, 100./2
 settings['Nt'], settings['Nr'] = 10, 3 
 settings['displacement'] = 45.
 settings['nFrames'] = 100
-settings['E'] = 200.e9
+settings['E'] = 200.e3
 settings['nu'] = .3
+settings['iteration'] = 10
 
 if node ==  'lcharleux':      
   abqlauncher   = '/opt/Abaqus/6.9/Commands/abaqus' # Local machine configuration
@@ -43,8 +44,8 @@ def read_file(file_name):
 
   for ligne in lignes:
       data = ligne.split() # Lines are splitted
-      force_exp.append(float(data[0]))
-      disp_exp.append(float(data[1]))
+      disp_exp.append(float(data[0]))
+      force_exp.append(float(data[1]))
   return -np.array(disp_exp), -np.array(force_exp)
 
 
@@ -98,7 +99,7 @@ class Simulation(object):
     m.PostProc()
     outputs = m.outputs
     force = -2. * outputs['history']['force']
-    disp = - 2 * outputs['history']['disp']
+    disp = -2 * outputs['history']['disp']
     
     self.disp = disp
     self.force = force
@@ -123,12 +124,13 @@ class Opti(object):
     self.sy = []
     self.n = []
     self.err = []
+    self.force_sim = []
     disp_exp, force_exp = read_file(self.settings['file_name'])
     g = interpolate.interp1d(disp_exp, force_exp)
     self.disp_exp = disp_exp
     self.force_exp = force_exp
     self.g = g
-    
+
   def Err(self, param):
     """
     Compute the residual error between experimental and simulated curve
@@ -150,17 +152,37 @@ class Opti(object):
     self.sy.append(sy)
     self.n.append(n)
     self.err.append(err)
+    self.force_sim.append(force_sim)
+    self.disp = disp
+    self.force_exp = force_exp
+    
+    
     return err
     
   def Optimize(self):
     p0 = [self.sy0, self.n0]
     
-    result = minimize(self.Err, p0, method='nelder-mead', options={'disp':True, 'maxiter':5})
+    result = minimize(self.Err, p0, method='nelder-mead', options={'disp':True, 'maxiter':settings['iteration']})
     self.result = result
     
-O = Opti(200.e6, 0.3, settings)
+O = Opti(200., 0.3, settings)
 O.Optimize()
 
+
+fig = plt.figure('Load vs. disp')
+plt.clf()
+
+plt.plot(O.disp, O.force_exp, 'k-', label = 'experimental curve', linewidth = 2.)
+plt.plot(O.disp, O.force_sim[0], 'g-', label = 'initial curve', linewidth = 2.)
+plt.plot(O.disp, O.force_sim[-1], 'r-', label = 'optimized curve', linewidth = 2.)
+for i in range(1, settings['iteration']):
+  plt.plot(O.disp, O.force_sim[i], 'b-', linewidth = .2)
+#plt.plot(disp.data[1], force.data[1], 'b-', label = 'Unloading', linewidth = 2.)  
+plt.legend(loc="upper left")
+plt.grid()
+plt.xlabel('Diplacement, $U$')
+plt.ylabel('Force, $F$')
+plt.savefig(workdir + label + '_load-vs-disp.pdf')
 
 
 #print s.force.data[0]
@@ -168,7 +190,6 @@ O.Optimize()
 f = s.Interp()
 x = np.arange(0., 49., 0.1)
 print f(x)
-
 """
 
 
