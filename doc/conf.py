4e9e346a6b2692eba0c11fca42c936653453b83c
Ludovic Charleux <ludovic.charleux@univ-savoie.fr>
2014-12-14 09:42:19 +0100

    Bug correction

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@
 import sys, os
 import mock
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', "matplotlib.sphinxext", "matplotlib.sphinxext.mathmp"]
 for mod_name in MOCK_MODULES:
   sys.modules[mod_name] = mock.Mock()
 
